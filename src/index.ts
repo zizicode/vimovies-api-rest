ff2ed372201ebc96796f0aa50ea0cf7cfadaeb07
zizicode <victordoblezz@gmail.com>
2026-06-09 22:48:15 -0400

    fix: add production URLs to CORS origins

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,8 @@ const app = new Hono()
 
 app.use('*', cors({
   origin: [
-    // 'https://www.vimovies.com',
+    'https://vimovies.com',
+    'https://www.vimovies.com',
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:3000',
